a54e56b73e9887b60432288ceffdc8f3ac601e27
Paulo <paulocesar147852@gmail.com>
2023-06-12 21:13:26 -0300

    changes-init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './styles.css'
+import Header from './Components/Header/index'
+
+function App() {
+ 
+
+  return (
+    <div className="main-Header">
+
+    <Header></Header>
+
+    </div>
+  )
+}
+
+
+export default App
---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,24 @@
+import './styles.css'
+import logo from'../../assets/icone trybe.png'
+
+function App() {
+ 
+
+  return (
+    <div className="NavBar">
+
+        <img src={logo} alt="Logomarca" />
+
+        <div className="T">T</div>
+        <div className="X">X</div>
+        <div className="wallet">WALLET</div>
+
+
+    
+
+    </div>
+  )
+}
+
+
+export default App
